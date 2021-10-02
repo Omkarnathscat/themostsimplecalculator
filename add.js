d7f654779d874c01bda0755636d12e464ac7fc95
Omkarnathscat <91599476+Omkarnathscat@users.noreply.github.com>
2021-10-02 16:53:34 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,11 @@
+var num1 = window.prompt("put in a number") ;
+var num2 = window.prompt ('put anothor number')
+// this is a database it is importent 
+// extreamly importent 
+// and i mean it ok!
+/* i hope my vars work */
+// maybe it won't but i hope it doese
+num1 = parseFloat(num1); 
+num2 = parseFloat(num2);
+document.write ( num1 + num2)
+// hello world is the first line of code of 89% of programers and coders ,but mine was ooga booga mamothmilk 
